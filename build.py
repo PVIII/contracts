ef83680853f22ce02c8fde075499dcb8673b1073
Patrick Wang-Freninger <github@freninger.at>
2019-02-17 22:17:43 +0100

    Update build files

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
-from cpt.packager import ConanMultiPackager
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bincrafters import build_template_boost_header_only
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder = build_template_boost_header_only.get_builder()
     builder.run()
